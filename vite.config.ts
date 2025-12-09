cc260bf9f146267bfd84bf6a6106da3094cc8466
Ousseynou Kandji <okanj18@gmail.com>
2025-12-09 20:51:46 +0000

    Fix Vercel sync and cleanup html

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Configuration Vite pour Vercel
 export default defineConfig({
   plugins: [react()],
   build: {
